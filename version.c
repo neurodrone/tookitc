83d701a1a3c38626a5a9e57313ec1d6bd49372ba
Vaibhav Bhembre <vaibhav@sailthru.com>
2013-09-28 10:31:02 -0400

    Add a trivial version check

---
+++ version.c
@@ -0,0 +1,13 @@
+/**
+ * @author Vaibhav Bhembre
+ * @version 2013/09/28
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    printf("C version: %ld\n", __STDC_VERSION__);
+    return 0;
+}
+
