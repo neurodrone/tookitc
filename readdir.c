6dddf30dddf02a9165145e3adc632327241c56ed
Vaibhav Bhembre <vaibhav@sailthru.com>
2013-10-26 00:00:15 -0400

    Add stat-specific information for each file.

--- readdir.c
+++ readdir.c
@@ -7,12 +7,13 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <assert.h>
-#include <errno.h>
 #include <inttypes.h>
+#include <sys/stat.h>
 
 int main(int argc, char *argv[]) {
     DIR *dirp;
     struct dirent *dp;
+    struct stat stat_buf;
 
     dirp = opendir(".");
     assert(dirp != NULL);
@@ -23,7 +24,10 @@ int main(int argc, char *argv[]) {
             break;
         }
 
-        printf("Name: %s, inode: %" PRIu64 "\n", dp->d_name, dp->d_ino);
+        printf("Name: %s, inode: %" PRIu64, dp->d_name, dp->d_ino);
+
+        stat(dp->d_name, &stat_buf);
+        printf(", size: %" PRIu64 " bytes.\n", stat_buf.st_size);
     } while (dp != NULL);
 
     closedir(dirp);
